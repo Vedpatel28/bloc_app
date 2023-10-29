94737e1436f999df35d8778c32504073c6ef7356
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-10-29 13:36:30 +0530

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:bloc_app/counter_bloc/counter_bloc.dart';
-import 'package:bloc_app/counter_bloc/counter_state.dart';
 import 'package:bloc_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(
-    // MultiBlocProvider(
-    //   providers: [
-    //     BlocProvider(
-    //       create: (context) => CounterController(),
-    //     ),
-    //   ],
-    //   child:
     const MyApp(),
-    // ),
   );
 }
 
